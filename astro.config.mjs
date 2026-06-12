bb26da6a3c00f0f1f267c36b1216c7fed20e1568
Maxime Huran <maxime.huran@gmail.com>
2026-06-12 15:21:14 +0200

    feat: add redirects and 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,25 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://maximehuran.github.io',
+    redirects: {
+        '/fr/pre-requis-pour-l-installation-de-votre-projet-sylius-2': '/',
+        '/fr/developpez-sous-sylius-2-en-40-minutes-top-chrono': '/',
+        '/fr/developpez-sous-sylius-en-40-minutes-top-chrono': '/',
+        '/fr/sylius-sans-e-commerce-pourquoi-et-comment': '/',
+        '/fr/creation-d-une-resource-avec-sylius-2': '/',
+        '/fr/deployer-sylius-sur-clever-cloud': '/',
+        '/media/gallery/pdfs/presentation.pdf': '/',
+        '/fr/installer-sylius-2': '/',
+        '/fr/mentions-legales': '/',
+        '/fr/site-click-and-collect': '/',
+        '/fr/pomodoro': '/',
+        '/fr/formation-sylius-2': '/',
+        '/fr/a-propos': '/',
+        '/fr/contact': '/',
+        '/fr/pre-requis-pour-la-creation-de-nos-ressources-sylius-2': '/',
+        '/fr/pwd': '/',
+        '/fr/site-vitrine': '/',
+        '/fr/site-e-commerce': '/',
+        '/fr/gmail-signature': '/',
+    },
 });
